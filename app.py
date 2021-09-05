106b92a1eb57e0ca93d2e22358e5138083255fbc
Charles Samuel R <rcharles.samuel99@gmail.com>
2021-09-05 14:53:54 +0530

    Moved to Dash

--- app.py
+++ app.py
@@ -1,66 +1,92 @@
-import gradio as gr
-import plotly.express as px
+import base64
+import datetime
+import io
+
+import dash
+from dash.dependencies import Input, Output, State
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_table
+
 import pandas as pd
 
-def get_plot(dataframe, x_axis, y_axis, plot_type):
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div([
+    dcc.Upload(
+        id='upload-data',
+        children=html.Div([
+            'Drag and Drop or ',
+            html.A('Select Files')
+        ]),
+        style={
+            'width': '100%',
+            'height': '60px',
+            'lineHeight': '60px',
+            'borderWidth': '1px',
+            'borderStyle': 'dashed',
+            'borderRadius': '5px',
+            'textAlign': 'center',
+            'margin': '10px'
+        },
+    ),
+    html.Div(id='output-data-upload'),
+])
+
+
+def parse_contents(contents, filename, date):
+    content_type, content_string = contents.split(',')
+
+    decoded = base64.b64decode(content_string)
     try:
-        df = pd.read_csv(dataframe.name)
-        if any(df.columns.str.contains('^Unnamed')):
-            df = pd.read_csv(dataframe.name, index_col=0)
+        if 'csv' in filename:
+            # Assume that the user uploaded a CSV file
+            df = pd.read_csv(
+                io.StringIO(decoded.decode('utf-8')))
+        elif 'xls' in filename:
+            # Assume that the user uploaded an excel file
+            df = pd.read_excel(io.BytesIO(decoded))
+    except Exception as e:
+        print(e)
+        return html.Div([
+            'There was an error processing this file.'
+        ])
 
-    except UnicodeDecodeError:
-        return None, "Upload a CSV file"
+    return html.Div([
+        html.H5(filename),
+        html.H6(datetime.datetime.fromtimestamp(date)),
 
-    if plot_type == 'Scatter':
-        try:
-            fig = px.scatter(df, x=x_axis, y=y_axis)
+        dash_table.DataTable(
+            data=df.to_dict('records'),
+            columns=[{'name': i, 'id': i} for i in df.columns]
+        ),
 
-            fig.write_html("output.html")
+        html.Hr(),  # horizontal line
 
-        except Exception:
-            return None, "One or both columns are not present"
+        # For debugging, display the raw contents provided by the web browser
+        html.Div('Raw Content'),
+        html.Pre(contents[0:200] + '...', style={
+            'whiteSpace': 'pre-wrap',
+            'wordBreak': 'break-all'
+        })
+    ])
 
-        return "output.html", "OK"
 
-    elif plot_type == "Bar":
-        try:
-            fig = px.bar(df, x=x_axis, y=y_axis)
+@app.callback(Output('output-data-upload', 'children'),
+              Input('upload-data', 'contents'),
+              State('upload-data', 'filename'),
+              State('upload-data', 'last_modified'))
+def update_output(list_of_contents, list_of_names, list_of_dates):
+    if list_of_contents is not None:
+        children = [
+            parse_contents(c, n, d) for c, n, d in
+            zip(list_of_contents, list_of_names, list_of_dates)]
+        return children
 
-            fig.write_html("output.html")
 
-        except Exception:
-            return None, "One or both columns are not present"
 
-        return "output.html", "OK"
-
-    elif plot_type == "Line":
-        try:
-            fig = px.line(df, x=x_axis, y=y_axis)
-
-            fig.write_html("output.html")
-
-        except Exception:
-            return None, "One or both columns are not present"
-
-        return "output.html", "OK"
-
-    elif plot_type == "Sunburst":
-        try:
-            fig = px.sunburst(df, parents=x_axis, values=y_axis)
-
-            fig.write_html("output.html")
-
-        except Exception:
-            return None, "One or both columns are not present"
-
-        return "output.html", "OK"
-
-iface = gr.Interface(get_plot, 
-        inputs=["file", "text", "text", 
-                gr.inputs.Dropdown(["Scatter", "Bar", "Line", "Sunburst"])],
-        outputs=[gr.outputs.File(label="Plot"), 
-                gr.outputs.Textbox(label="Message")], 
-        examples=[["datasets/iris_data.csv", "sepal width", "sepal length", "Scatter"]])
-
-if __name__ == "__main__":
-    iface.launch()
+if __name__ == '__main__':
+    app.run_server(debug=True)
