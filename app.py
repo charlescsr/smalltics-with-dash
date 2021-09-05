d490cf51a1da7c84f3492f23cd218c90040288cd
Charles Samuel R <rcharles.samuel99@gmail.com>
2021-09-05 14:49:56 +0530

    Added dataset for testing

--- app.py
+++ app.py
@@ -5,23 +5,22 @@ import pandas as pd
 def get_plot(dataframe, x_axis, y_axis, plot_type):
     try:
         df = pd.read_csv(dataframe.name)
+        if any(df.columns.str.contains('^Unnamed')):
+            df = pd.read_csv(dataframe.name, index_col=0)
 
     except UnicodeDecodeError:
         return None, "Upload a CSV file"
 
     if plot_type == 'Scatter':
         try:
-            fig = px.bar(df, x=x_axis, y=y_axis)
+            fig = px.scatter(df, x=x_axis, y=y_axis)
 
             fig.write_html("output.html")
-
-            with open("output.html") as html:
-                html_string = html.read()
 
         except Exception:
             return None, "One or both columns are not present"
 
-        return html_string, "OK"
+        return "output.html", "OK"
 
     elif plot_type == "Bar":
         try:
@@ -29,47 +28,39 @@ def get_plot(dataframe, x_axis, y_axis, plot_type):
 
             fig.write_html("output.html")
 
-            with open("output.html") as html:
-                html_string = html.read()
-
         except Exception:
             return None, "One or both columns are not present"
 
-        return html_string, "OK"
+        return "output.html", "OK"
 
     elif plot_type == "Line":
         try:
-            fig = px.bar(df, x=x_axis, y=y_axis)
+            fig = px.line(df, x=x_axis, y=y_axis)
 
             fig.write_html("output.html")
-
-            with open("output.html") as html:
-                html_string = html.read()
 
         except Exception:
             return None, "One or both columns are not present"
 
-        return html_string, "OK"
+        return "output.html", "OK"
 
     elif plot_type == "Sunburst":
         try:
-            fig = px.bar(df, x=x_axis, y=y_axis)
+            fig = px.sunburst(df, parents=x_axis, values=y_axis)
 
             fig.write_html("output.html")
-
-            with open("output.html") as html:
-                html_string = html.read()
 
         except Exception:
             return None, "One or both columns are not present"
 
-        return html_string, "OK"
+        return "output.html", "OK"
 
 iface = gr.Interface(get_plot, 
         inputs=["file", "text", "text", 
                 gr.inputs.Dropdown(["Scatter", "Bar", "Line", "Sunburst"])],
-        outputs=[gr.outputs.HTML(label="Plot"), 
-                gr.outputs.Texbox(label="Message")])
+        outputs=[gr.outputs.File(label="Plot"), 
+                gr.outputs.Textbox(label="Message")], 
+        examples=[["datasets/iris_data.csv", "sepal width", "sepal length", "Scatter"]])
 
 if __name__ == "__main__":
     iface.launch()
